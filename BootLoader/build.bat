04e535881a35fcf4a45ba6118d5465284b8d6673
DeDf <1815349357@qq.com>
2017-04-23 02:59:35 +0800

    aaa

--- BootLoader/build.bat
+++
@@ -1,9 +0,0 @@
-gcc -c bootasm.S  -o bootasm.o
-gcc -O2 -c bootmain.c -o bootmain.o
-
-ld -Ttext 0x7c00 -Tdata 0x7de0 bootasm.o bootmain.o -o bootloader_t
-
-objcopy -O binary -j .text -j .data bootloader_t bootloader
-
-rem copy /b boot_sect.bin+kernel.bin os_image
-PAUSE
